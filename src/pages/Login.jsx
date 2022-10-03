dee090c84d8e6a8aa9c1094a0b6586429f58d2ee
Matheus Bezerra <mabezerra2@gmail.com>
2022-10-03 14:42:50 -0400

    feat(Page Login): Feito formulario
    foi feito o formulario e validadção do mesmo, e criado um componente Loading

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../component/Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  state = {
+    name: '',
+    loading: true,
+    redirect: false,
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  buttonForm = async () => {
+    const { name } = this.state;
+    this.setState({ loading: false });
+    await createUser({ name });
+    this.setState({ loading: true, redirect: true });
+  };
+
   render() {
+    const {
+      name,
+      loading,
+      redirect,
+    } = this.state;
+    const magaicNumber = 3;
+    const validName = name.length >= magaicNumber;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="name">
+            <input
+              type="text"
+              name="name"
+              value={ name }
+              onChange={ this.handleChange }
+              data-testid="login-name-input"
+              placeholder="digite seu nome"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ !validName }
+            onClick={ this.buttonForm }
+          >
+            Entrar
+          </button>
+          {!loading && <Loading />}
+          {redirect && <Redirect to="/search" />}
+        </form>
+      </div>
     );
   }
 }
